ba8300939042607ca20b93d5a67b90187db356cc
Pawel Rybowicz <pawel.rybowicz.programista@gmail.com>
2022-06-13 01:03:15 +0200

    Add MyBudget all options

---
+++ MyBudget.h
@@ -0,0 +1,38 @@
+#ifndef MYBUDGET_H
+#define MYBUDGET_H
+
+#include <iostream>
+#include "UserManager.h"
+#include "IncomeManager.h"
+#include "ExpenseManager.h"
+#include "BalanceManager.h"
+
+using namespace std;
+
+class MyBudget {
+
+    IncomeManager incomeManager;
+    ExpenseManager expenseManager;
+    UserManager userManager;
+
+    char ChoseOptionFromDateMenuForIncome();
+    char ChoseOptionFromDateMenuForExpense();
+
+
+public:
+
+    char ChooseOptionFromMainMenu ();
+    char ChooseOptionFromUserMenu();
+    void UserRegistration();
+    void UserLogin();
+    void ChangeLoggedUserPassword();
+    void UserLogout();
+    bool IsUserLoggedIn();
+    void CalculateCurrentMonthBalance();
+    void CalculateLastMonthBalance();
+    void CalculateSelectedPeriodBalance();
+    void AddIncome();
+    void AddExpense();
+
+};
+#endif // MYBUDGET_H
